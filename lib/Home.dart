ff6ea5035450c9e6afdaf5935308fbab97d1346d
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2020-01-23 22:17:19 -0300

    Utilizando o widget BottonNavigationBar

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/telas/Biblioteca.dart';
+import 'package:youtube/telas/EmAlta.dart';
+import 'package:youtube/telas/Inicio.dart';
+import 'package:youtube/telas/Inscricoes.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,6 +10,16 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int _indiceAtual = 0;
+  List<Widget> _telas = [
+    Inicio(),
+    EmAlta(),
+    Inscricoes(),
+    Biblioteca(),
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,7 +51,44 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      body: Container(),
+      body: Container(
+        child: Center(
+            child: _telas[_indiceAtual]
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _indiceAtual,
+        onTap: (indice){
+        setState(() {
+          _indiceAtual = indice;
+        });
+        },
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.red,
+        items: [
+          BottomNavigationBarItem(
+            //backgroundColor: Colors.orange,
+            title: Text("Home"),
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+              //backgroundColor: Colors.yellow,
+              title: Text("Em alta"),
+              icon: Icon(Icons.whatshot)
+          ),
+          BottomNavigationBarItem(
+              //backgroundColor: Colors.blue,
+              title: Text("Inscrições"),
+              icon: Icon(Icons.video_library)
+          ),
+          BottomNavigationBarItem(
+              //backgroundColor: Colors.green,
+              title: Text("Biblioteca"),
+              icon: Icon(Icons.folder)
+          ),
+        ],
+      ),
+
     );
   }
 }
